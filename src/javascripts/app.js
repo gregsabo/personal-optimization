9fab44d4dc032e27bf2fd5d258fcdaa3e194c0bc
Greg Sabo <gregsabo@stripe.com>
2015-11-11 21:38:59 -0800

    Hey it's a cube

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,19 +1,61 @@
-import './asyncModules'
-import exclaimify from './exclaimify'
-
-const button = document.getElementById('button')
+/* esnext: true */
+import './asyncModules';
+import exclaimify from './exclaimify';
+import THREE from 'three';
 
 const alertAsyncMessage = function() {
   // CommonJS async syntax webpack magic
   require.ensure([], function() {
-    const message = require("./asyncMessage")
-    alert(exclaimify(message))
-  })
+    const message = require("./asyncMessage");
+    alert(exclaimify(message));
+  });
+};
+
+var camera, scene, renderer;
+var mesh;
+
+init();
+animate();
+
+function init() {
+
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.z = 400;
+
+  scene = new THREE.Scene();
+
+
+  var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+  var material = new THREE.MeshBasicMaterial( { color: "white", overdraw: 0.5 } );
+
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
-console.log(`
-  asset references like this one:
-    images/gulp.png
-  get updated in js too!`)
+function onWindowResize() {
 
-button.addEventListener('click', alertAsyncMessage)
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  mesh.rotation.x += 0.005;
+  mesh.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
